d2e1e911b3b9b416beab9efdcd6a052bfd38d2f2
sanjayr2 <ranjeethsanjay@yahoo.com>
2020-02-11 18:56:10 +0200

    New Label Added by sanjay2 (member2)

--- WindowsFormsApplicationGitCollabDemoSanjayMain/Form1.Designer.cs
+++ WindowsFormsApplicationGitCollabDemoSanjayMain/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,13 +41,23 @@
             this.label1.Size = new System.Drawing.Size(1079, 64);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to Git Demo from Sanjay_Main";
-            //this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(524, 64);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Member 2 sanjayr2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1320, 769);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
